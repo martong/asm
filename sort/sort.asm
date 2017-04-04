497e7e3c8db60dfe93729e97174b74da1720e17f
Gabor Marton <martongabesz@gmail.com>
2017-04-04 20:57:06 +0200

    Add loop over range

--- sort/sort.asm
+++ sort/sort.asm
@@ -28,6 +28,21 @@ display_one_digit:
     add rsp, 8
     ret
 
+; rdi: pointer to range of bytes
+; rsi: len of range
+loop_over_range:
+loop:
+    push rdi
+    push rsi
+    call display_one_digit
+    call newline
+    pop rsi
+    pop rdi
+    inc rdi
+    dec rsi
+    jnz loop
+    ret
+
 start:
     mov     rax, 0x2000004 ; write
     mov     rdi, 1 ; stdout
@@ -38,6 +53,10 @@ start:
     mov rdi, x
     call display_one_digit
     call newline
+
+    mov rdi, x
+    mov rsi, 5
+    call loop_over_range
 
     mov     rax, 0x2000001 ; exit
     mov     rdi, 0
@@ -50,5 +69,5 @@ msg:    db      "Hello, world!", 10
 .len:   equ     $ - msg
 
 NEWLINE: db 0x0a ; http://www.asmcommunity.net/forums/topic/?id=26085
-x: dw 1, 2, 3, 4, 5
+x: db 1, 2, 3, 4, 5
 xx: dw '1'
