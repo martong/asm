b5fb23ae55b39452f1afa1a0fc21bbfabe947d77
Gabor Marton <martongabesz@gmail.com>
2017-04-04 20:37:34 +0200

    Add newline

--- sort/sort.asm
+++ sort/sort.asm
@@ -4,6 +4,15 @@ global start
 
 section .text
 
+newline:
+    mov     rax, 0x2000004 ; write
+    mov     rdi, 1 ; stdout
+    mov     rsi, NEWLINE ; msg
+    mov     rdx, 1 ; len
+    syscall
+    ret
+
+
 display_one_digit:
     ; save param from rdi
     mov r15, [rdi]
@@ -28,6 +37,7 @@ start:
 
     mov rdi, x
     call display_one_digit
+    call newline
 
     mov     rax, 0x2000001 ; exit
     mov     rdi, 0
@@ -39,5 +49,6 @@ section .data
 msg:    db      "Hello, world!", 10
 .len:   equ     $ - msg
 
+NEWLINE: db 0x0a ; http://www.asmcommunity.net/forums/topic/?id=26085
 x: dw 1, 2, 3, 4, 5
 xx: dw '1'
