4f6eea5cbc9887ae6e5f08b0e21057cc1ce34394
Gabor Marton <martongabesz@gmail.com>
2017-04-05 11:32:21 +0200

    Add test_find_smallest_in_range

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#define ASSERT(CONDITION)                                                      \
+    do                                                                         \
+        if (!(CONDITION)) {                                                    \
+            printf("Assertion failure %s:%d ASSERT(%s)\n", __FILE__, __LINE__, \
+                   #CONDITION);                                                \
+            abort();                                                           \
+        }                                                                      \
+    while (0)
 
 extern "C" {
 char* find_smallest_in_range(char* range, size_t len);
 }
 
+void test_find_smallest_in_range() {
+    {
+        char a[] = {1, 2, 3};
+        char* res = find_smallest_in_range(a, 3);
+        ASSERT(res == a);
+        ASSERT(*res == 1);
+    }
+
+    {
+        char a[] = {1, 2, 0, 4};
+        char* res = find_smallest_in_range(a, 4);
+        ASSERT(res == a + 2);
+        ASSERT(*res == 0);
+    }
+
+    {
+        char a[] = {1, 2, -3};
+        char* res = find_smallest_in_range(a, 3);
+        ASSERT(res == a + 2);
+        ASSERT(*res == -3);
+    }
+}
+
 int main() {
-    char a[] = {1,2,0,4};
-    char* res = find_smallest_in_range(a, 4);
-    std::cout << (int)*res << std::endl;
+    test_find_smallest_in_range();
     return 0;
 }
+
